7cb396b96a0938d10d5260de8dc2c1703b3a4415
Kian Kasad <kdkasad@gmail.com>
2020-07-14 14:49:03 -0700

    [polybar] update polybar config

--- config/polybar/start-polybar.sh
+++ config/polybar/start-polybar.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 while ! pgrep -x dwm
-do :
+do
+	sleep 0.05
 done
+
+sleep 0.04
 
 for monitor in $(polybar --list-monitors | cut -d: -f1)
 do
-	MONITOR=$monitor polybar -r ${POLYBAR_BAR:-statusbar} &
+	MONITOR=$monitor polybar ${POLYBAR_BAR:-statusbar} &
 done
