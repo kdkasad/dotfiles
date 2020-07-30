b3debfd38624d094622976477fe976939566af96
Kian Kasad <kian@kasad.com>
2020-07-30 15:32:07 -0700

    [polybar] automatically detect thermal zone when starting polybar

--- config/polybar/start-polybar.sh
+++ config/polybar/start-polybar.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+## wait for dwm
+
 while ! pgrep -x dwm
 do
 	sleep 0.05
 done
-
 sleep 0.04
+
+## find which thermal zone to monitor
+
+# set $zone to the correct thermal zone
+zone="$(grep -m1 ${THERMAL_ZONE_TYPE:-pkg_temp} /sys/class/thermal/thermal_zone*/type | grep -o "thermal_zone[0-9]\+" | sed 's/thermal_zone//')"
+# update thermal_zone in polybar config
+sed -i "/thermal-zone/s/[0-9]\+/$zone/" ${XDG_CONFIG_HOME:-$HOME/.config}/polybar/config
+
+## start polybar(s)
 
 for monitor in $(polybar --list-monitors | cut -d: -f1)
 do
