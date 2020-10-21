6c243c9ccab9009c497134ccf73ac39875c1fd48
Kian Kasad <kian@kasad.com>
2020-10-21 10:03:55 -0700

    update update-repo script

--- update-repo.sh
+++ update-repo.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-find . -not -path '*/.git*' -type f -print0 | sed -z 's|^\./||' | grep -zvF -e 'LICENSE' -e 'update-repo.sh' | xargs -r0I%p ln -fv "$HOME/.%p" "./%p"
+find . -not -path '*/.git*' -not -name 'update-repo.sh' -not -name 'LICENSE' -type f -print0 \
+	| sed -z 's|^\./||' \
+	| xargs -r0I%p ln -fv "$HOME/.%p" "./%p"
