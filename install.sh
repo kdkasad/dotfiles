e31a760ab6712b1cedb7a78b6a889d3b818781d4
Kian Kasad <kian@kasad.com>
2020-11-07 15:12:30 -0800

    improve install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo creating directories... >&2
-find -not -path '*/.git*' -type d | sed 's/^\.\///' | xargs -rI%p mkdir -p "$HOME/.%p"
+find -not -path '*/.git*' -prune -type d | sed 's/^\.\///' | xargs -rI%p mkdir -p "$HOME/.%p"
 
 echo linking files... >&2
-find -not -path '*/.git*' -type f | sed 's/^\.\///' | xargs -rI%p ln ./%p "$HOME/.%p"
+find -not \( -path '*/.git*' -prune -o -name 'LICENSE' -o -name 'install.sh' -o -name 'update-repo.sh' \) -type f | sed 's/^\.\///' | xargs -rI%p ln ./%p "$HOME/.%p"
 
 echo 'Done!' >&2
