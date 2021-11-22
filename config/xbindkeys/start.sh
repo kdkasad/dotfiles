607ec43728fb4f9e17dba0c33618aede14b153c9
Kian Kasad <kian@kasad.com>
2021-11-22 11:21:09 -0800

    [xinit][keymap] change the way xbindkeys(1) is started
    * Multiple separate keymaps can be defined in ~/.config/xbindkeys
    	* Each one ends with the .keys extension
    * The ~/.config/xbindkeys/start.sh script starts xbindkeys
    	* By default it starts one instance for each keymap
    	* Specific keymaps can be specified on the command line

---
+++ config/xbindkeys/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+
+# kill existing pkill(1) instances
+pkill -x xbindkeys
+
+# if keymaps specified, start only those
+if [ $# -ge 1 ]; then
+	for map in "$@"; do
+		setsid -f xbindkeys -f "$dir/${map%.keys}.keys"
+		exit
+	done
+	exit
+fi
+
+# if none specified, start all
+find "$dir" -type f -name '*.keys' -exec setsid -f xbindkeys -f {} \;
