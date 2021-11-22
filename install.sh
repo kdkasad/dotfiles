fbbccec13f7ba58e9919d00c996eedbc192099f8
Kian Kasad <kian@kasad.com>
2021-11-22 11:32:06 -0800

    make install script verbose

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo creating directories... >&2
-find -not \( -name '.git*' -prune \) -type d | sed 's/^\.\///' | xargs -rI%p mkdir -p "$HOME/.%p"
+find -not \( -name '.git*' -prune \) -type d | sed 's/^\.\///' | xargs -rI%p mkdir -vp "$HOME/.%p"
 
 echo linking files... >&2
-find -not \( -name '.git*' -prune -o -name 'LICENSE' -o -name 'install.sh' -o -name 'update-repo.sh' \) -type f | sed 's/^\.\///' | xargs -rI%p ln ./%p "$HOME/.%p"
+find -not \( -name '.git*' -prune -o -name 'LICENSE' -o -name 'install.sh' -o -name 'update-repo.sh' \) -type f | sed 's/^\.\///' | xargs -rI%p ln -v ./%p "$HOME/.%p"
 
 echo 'Done!' >&2
