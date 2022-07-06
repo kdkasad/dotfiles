0afa892fe04d3fe0b06c8f3dd59064c0efd01d3b
Kian Kasad <kian@kasad.com>
2022-07-05 17:10:35 -0700

    Don't update log files in repository

--- update-repo.sh
+++ update-repo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-find . -not \( -name '.git*' -prune -o -name 'update-repo.sh' -o -name 'install.sh' -o -name 'LICENSE' \) -type f -print0 \
+find . -not \( -name '.git*' -prune -o -name 'update-repo.sh' -o -name 'install.sh' -o -name 'LICENSE' -o -path './local/log/*/log' \) -type f -print0 \
 	| sed -z 's|^\./||' \
 	| xargs -r0I%p ln -fv "$HOME/.%p" "./%p"
