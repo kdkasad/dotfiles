bc4903a14dd3f551c1611bf10c84d8f92ba568da
Kian Kasad <kian@kasad.com>
2024-10-07 20:10:57 -0400

    [nvim] Add C-style comment snippet

--- .config/nvim/lua/snippets.lua
+++ .config/nvim/lua/snippets.lua
@@ -1,3 +1,6 @@
 -- Auto-correct "ture" and "treu" to "true"
 vim.keymap.set("ia", "ture", "true")
 vim.keymap.set("ia", "treu", "true")
+
+-- Easy C-style comments
+vim.keymap.set("ia", "com", "/* */<left><left><left>")
