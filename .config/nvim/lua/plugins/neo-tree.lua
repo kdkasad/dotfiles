af59e15f4e838acec41ca3e71f6f6e5d1a7491f0
Kian Kasad <kian@kasad.com>
2024-11-01 19:55:55 -0400

    [nvim] Close neo-tree when opening a file

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,4 +7,16 @@ return {
         -- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
     },
+    config = function()
+        require("neo-tree").setup({
+            event_handlers = {
+                {
+                    event = "file_opened",
+                    handler = function(file_path)
+                        require("neo-tree.command").execute({ action = "close" })
+                    end
+                },
+            }
+        })
+    end
 }
