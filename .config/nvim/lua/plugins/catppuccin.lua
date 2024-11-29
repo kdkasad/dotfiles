1f2d24f6cc7ec2be7325adb5db6da7f910d48064
Kian Kasad <kian@kasad.com>
2024-11-29 14:01:57 -0500

    nvim: Add bg.nvim plugin
    Sets the background color of the terminal when changing Neovim's
    colorscheme.

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,19 +1,25 @@
 -- Catppuccin color scheme
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    opts = {
-        flavour = auto,
-        transparent_background = false,
-        background = {
-            light = "latte",
-            dark = "mocha",
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            flavour = auto,
+            transparent_background = false,
+            background = {
+                light = "latte",
+                dark = "mocha",
+            },
         },
+        config = function(_, opts)
+            require("catppuccin").setup(opts)
+            vim.cmd.colorscheme("catppuccin")
+        end,
     },
-    config = function(_, opts)
-        require("catppuccin").setup(opts)
-        vim.cmd.colorscheme("catppuccin")
-    end,
+    {
+        "typicode/bg.nvim",
+        lazy = false
+    },
 }
