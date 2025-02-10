831c15269efb5d120447fe850602ee82807ab5ef
Kian Kasad <kian@kasad.com>
2025-02-09 20:37:37 -0500

    nvim: Add breadcrumbs plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -16,6 +16,17 @@ return {
         },
     },
 
+    -- Breadcrumbs bar
+    {
+        'Bekaboo/dropbar.nvim',
+        config = function()
+            local dropbar_api = require('dropbar.api')
+            vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+            vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+            vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+        end
+    },
+
     -- folke's snacks
     {
         "folke/snacks.nvim",
