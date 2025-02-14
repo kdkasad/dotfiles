5c854b4429f36d0ef7d1e2253b1a11266e45c961
Kian Kasad <kian@kasad.com>
2025-02-14 13:16:35 -0500

    nvim: Change keybindings to scroll popup docs in completions

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -19,8 +19,8 @@ return {
                 },
                 mapping = cmp.mapping.preset.insert({
                     -- TODO: Put these in keymap.lua
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-j>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
