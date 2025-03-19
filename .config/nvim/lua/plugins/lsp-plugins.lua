b86947b4a10720a238a8616e3242d072eda7e79d
Kian Kasad <kian@kasad.com>
2025-03-19 16:55:51 -0700

    nvim: Get better LSP support for Neovim config files

--- .config/nvim/lua/plugins/lsp-plugins.lua
+++ .config/nvim/lua/plugins/lsp-plugins.lua
@@ -86,13 +86,15 @@ return {
                                 },
                                 -- Make the server aware of Neovim runtime files
                                 workspace = {
-                                    -- checkThirdParty = false,
-                                    library = {
-                                        vim.env.VIMRUNTIME,
-                                        -- Depending on the usage, you might want to add additional paths here.
-                                        -- "${3rd}/luv/library"
-                                        -- "${3rd}/busted/library",
-                                    },
+                                    checkThirdParty = false,
+                                    -- Faster but doesn't include plugin configs
+                                    -- library = {
+                                    --     vim.env.VIMRUNTIME,
+                                    --     -- Depending on the usage, you might want to add additional paths here.
+                                    --     -- "${3rd}/luv/library"
+                                    --     -- "${3rd}/busted/library",
+                                    -- },
+                                    library = vim.api.nvim_get_runtime_file("", true),
                                 },
                             })
                         end,
