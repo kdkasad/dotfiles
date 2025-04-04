543ddd7029bae9c63bd6aaa87a53c2d18daf02ac
Kian Kasad <kian@kasad.com>
2025-04-04 18:38:07 -0400

    nvim: Use clippy for Rust linting

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,3 +1,15 @@
+vim.g.rustaceanvim = {
+    server = {
+        default_settings = {
+            ["rust-analyzer"] = {
+                checkOnSave = {
+                    command = "clippy",
+                }
+            }
+        }
+    }
+}
+
 return {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
