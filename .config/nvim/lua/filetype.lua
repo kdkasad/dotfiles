6700ee9c43dca97a1162f04039f22157e96106f0
Kian Kasad <kian@kasad.com>
2025-04-09 13:20:25 -0400

    nvim: Add custom function to create files for MA 341 homework

--- .config/nvim/lua/filetype.lua
+++ .config/nvim/lua/filetype.lua
@@ -100,4 +100,26 @@ vim.api.nvim_create_autocmd("FileType", {
     end
 })
 
+-- Keybinding to create files for MA 341 homework
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "typst",
+    callback = function()
+        vim.keymap.set("n", "<leader>k3", function()
+            local filename = vim.fn.expand("<cfile>")
+            if filename:len() == 0 then
+                vim.notify("No filename under cursor", vim.log.levels.ERROR)
+                return
+            end
+            if filename:sub(-4) ~= ".typ" then
+                vim.notify("Filename doesn't end with .typ", vim.log.levels.ERROR)
+                return
+            end
+            local problem = filename:sub(0, -5)
+            vim.cmd.edit(vim.fn.expand("%:h") .. "/" .. filename)
+            local expand_abbr_key = vim.api.nvim_replace_termcodes("<C-]>", true, false, true)
+            vim.cmd.normal("i341prob" .. expand_abbr_key .. problem)
+        end)
+    end,
+})
+
 -- vim: ft=lua sw=4 ts=4 et fdm=marker fmr={{{,}}} foldlevel=2
