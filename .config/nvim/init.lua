05a279e91c5b7e7c1f06860f2b4380a2bd912383
Kian Kasad <kian@kasad.com>
2025-04-18 15:14:22 -0400

    nvim: Remove pcalls in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,49 +1,39 @@
 -- Load Vim settings/options
-pcall(function()
-    require("settings")
-end)
+require("settings")
 
 -- Load plugins (using lazy.nvim) {{{
-pcall(function()
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not (vim.uv or vim.loop).fs_stat(lazypath) then
-        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-        if vim.v.shell_error ~= 0 then
-            vim.api.nvim_echo({
-                { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-                { out,                            "WarningMsg" },
-                { "\nPress any key to exit..." },
-            }, true, {})
-            vim.fn.getchar()
-            os.exit(1)
-        end
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
     end
-    vim.opt.rtp:prepend(lazypath)
-    require("lazy").setup({
-        spec = {
-            { import = "plugins" },
-        },
-        ui = {
-            border = "rounded",
-        },
-    })
-end)
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
+    ui = {
+        border = "rounded",
+    },
+})
 -- }}}
 
 -- Load keybindings
-pcall(function()
-    require("keymap")
-end)
+require("keymap")
 
 -- Load filetype-specific settings
-pcall(function()
-    require("filetype")
-end)
+require("filetype")
 
 -- Load snippets
-pcall(function()
-    require("snippets")
-end)
+require("snippets")
 
 -- vim: ft=lua sw=4 ts=4 et fdm=marker fmr={{{,}}} foldlevel=2
