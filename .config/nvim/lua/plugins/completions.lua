ccdff24b1efcfb87e4c30effba41e985fa44c280
Kian Kasad <kian@kasad.com>
2025-04-18 16:57:11 -0400

    nvim: Enable ghost text for completions
    This feature is marked as experimental in nvim-cmp, so it may get
    disabled soon.

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -11,42 +11,48 @@ return {
         event = { "InsertEnter" },
         config = function()
             local cmp = require("cmp")
-            cmp.setup({
-                snippet = {
-                    -- REQUIRED - you must specify a snippet engine
-                    expand = function(args)
-                        vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    -- TODO: Put these in keymap.lua
-                    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-j>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                    ["<C-d>"] = function()
-                        if cmp.visible_docs() then
-                            cmp.close_docs()
-                        else
-                            cmp.open_docs()
-                        end
-                    end,
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    -- { name = 'vsnip' }, -- For vsnip users.
-                    -- { name = 'luasnip' }, -- For luasnip users.
-                    -- { name = 'ultisnips' }, -- For ultisnips users.
-                    -- { name = 'snippy' }, -- For snippy users.
-                }, {
-                    { name = "buffer" },
-                }),
-            })
+            cmp.setup(
+                ---@type cmp.ConfigSchema
+                {
+                    snippet = {
+                        -- REQUIRED - you must specify a snippet engine
+                        expand = function(args)
+                            vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                        end,
+                    },
+                    window = {
+                        completion = cmp.config.window.bordered(),
+                        documentation = cmp.config.window.bordered(),
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        -- TODO: Put these in keymap.lua
+                        ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+                        ["<C-j>"] = cmp.mapping.scroll_docs(4),
+                        ["<C-Space>"] = cmp.mapping.complete(),
+                        ["<C-e>"] = cmp.mapping.abort(),
+                        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                        ["<C-d>"] = function()
+                            if cmp.visible_docs() then
+                                cmp.close_docs()
+                            else
+                                cmp.open_docs()
+                            end
+                        end,
+                    }),
+                    sources = cmp.config.sources({
+                        { name = "nvim_lsp" },
+                        -- { name = 'vsnip' }, -- For vsnip users.
+                        -- { name = 'luasnip' }, -- For luasnip users.
+                        -- { name = 'ultisnips' }, -- For ultisnips users.
+                        -- { name = 'snippy' }, -- For snippy users.
+                    }, {
+                        { name = "buffer" },
+                    }),
+                    experimental = {
+                        ghost_text = true,
+                    },
+                }
+            )
         end,
     },
 }
