a94031cea2006c35f31481bb045e42bcbce10bb3
Kian Kasad <kian@kasad.com>
2025-05-12 10:22:09 -0700

    nvim: Update rust-analyzer settings

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -2,7 +2,7 @@ vim.g.rustaceanvim = {
     server = {
         default_settings = {
             ["rust-analyzer"] = {
-                checkOnSave = {
+                check = {
                     command = "clippy",
                 }
             }
