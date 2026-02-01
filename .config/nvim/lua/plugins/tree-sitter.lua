ae6ec4ad76af66f53c76962bfbb5ba60ce4b2e88
Kian Kasad <kian@kasad.com>
2026-02-01 16:39:04 -0500

    fix(nvim): update nvim-treesitter config for latest version

--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -1,37 +1,8 @@
--- List of parsers to install
-local ensure_installed_parsers = {
-    -- "lua",
-    -- "javascript",
-    -- "python",
-    -- "c",
-    -- "vim",
-    -- "vimdoc",
-    -- "html",
-    -- "css",
-    -- "make",
-    -- "bash",
-}
-
 return {
     -- Tree-sitter (for syntax highlighting & indenting)
     {
         "nvim-treesitter/nvim-treesitter",
-        lazy = true,
-        event = LazyFile,
-        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        lazy = false,
         build = ":TSUpdate",
-        config = function()
-            -- Set up parsers
-            local tsconfig = require("nvim-treesitter.configs")
-            tsconfig.setup({
-                ensure_installed = ensure_installed_parsers,
-                auto_install = true,
-                highlight = { enable = true },
-                indent = {
-                    enable = true,
-                    disable = { "typst" },
-                },
-            })
-        end,
     },
 }
